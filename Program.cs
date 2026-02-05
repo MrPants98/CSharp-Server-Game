7d4428a054a710af5bf3903061fb6b1c2a58cff3
Oliver Cecil <ocecil98@gmail.com>
2026-02-05 17:39:13 -0500

    Added Basic Websocket functionality

--- Program.cs
+++ Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var server = new WebsocketServer();
+        
+        server.InitWebsocketServer("localhost", 8080);
+        
+        do
+        {
+            var line = Console.ReadLine();
+            if (line != "stop") 
+                continue;
+            
+            server.CloseWebsocketServer();
+            break;
+        } while (true);
     }
 }
--- WebsocketServer.cs
+++ WebsocketServer.cs
@@ -1,6 +1,111 @@
-﻿namespace Csharp_game_server;
+﻿using System.Collections.Concurrent;
+using System.Net;
+using System.Net.WebSockets;
+
+namespace Csharp_game_server;
 
 public class WebsocketServer
 {
     
+    private readonly ConcurrentDictionary<Guid, WebSocket> _connectedClients = new();
+    
+    private readonly HttpListener _listener = new();
+    private CancellationTokenSource _cts = new();
+
+    public async Task InitWebsocketServer(string ip, int port) {
+        if (!HttpListener.IsSupported) {
+            Console.WriteLine("Needs Windows XP SP2, Server 2003 or later.");
+            return;
+        }
+
+        var uri = $"http://{ip}:{port}/";
+        _listener.Prefixes.Add(uri);
+        
+        _listener.Start();
+        Console.WriteLine($"Listening on {uri}");
+
+        while (!_cts.IsCancellationRequested) {
+            var context = await _listener.GetContextAsync();
+            if (context.Request.IsWebSocketRequest)
+            {
+                _ = ProcessClientJoining(context);
+            }
+            else {
+                context.Response.StatusCode = 400;
+                context.Response.Close();
+            }
+        }
+    }
+
+    public async Task CloseWebsocketServer()
+    {
+        await _cts.CancelAsync();
+        
+        foreach (var client in _connectedClients)
+        {
+            await client.Value.CloseAsync(WebSocketCloseStatus.NormalClosure, "Server Closing", CancellationToken.None);
+            client.Value.Dispose();
+        }
+        _listener.Stop();
+        _listener.Close();
+    }
+
+    private async Task ProcessClientJoining(HttpListenerContext context)
+    {
+
+        WebSocket? socket = null;
+        var clientId = Guid.NewGuid();
+
+        try
+        {
+            var socketContext = await context.AcceptWebSocketAsync(null);
+            socket = socketContext.WebSocket;
+            _connectedClients[clientId] = socket;
+            Console.WriteLine($"Client {clientId} Connected at ip {context.Request.RemoteEndPoint.Address.ToString()}");
+
+            await ReceiveLoop(socket);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Client error: {e.Message}");
+        }
+        finally
+        {
+            if (socket != null)
+            {
+                _connectedClients.TryRemove(clientId, out _);
+
+                if (socket.State == WebSocketState.Open || socket.State == WebSocketState.CloseReceived)
+                    await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Disconnect", CancellationToken.None);
+                
+                socket.Dispose();
+                Console.WriteLine($"Client {clientId} disconnected");
+            }
+        }
+    }
+
+    private async Task ReceiveLoop(WebSocket socket)
+    {
+        var buffer = new byte[512];
+        var fullPacket =  new List<byte>();
+
+        while (socket.State == WebSocketState.Open)
+        {
+            WebSocketReceiveResult result;
+            do
+            {
+                result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                fullPacket.AddRange(buffer.Take(result.Count));
+            } while (!result.EndOfMessage);
+
+            if (result.MessageType == WebSocketMessageType.Binary)
+            {
+                
+            }
+            
+            await socket.SendAsync(new ArraySegment<byte>(fullPacket.ToArray()), WebSocketMessageType.Text, true, CancellationToken.None);
+            
+            fullPacket.Clear();
+        }
+    }
 }
